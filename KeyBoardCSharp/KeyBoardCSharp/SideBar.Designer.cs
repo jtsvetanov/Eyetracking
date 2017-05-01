07610dfca81372126d49e75d1b68dfde11b6e4fb
JORDAN\Jor <jtsvet2@uic.edu>
2017-05-01 08:38:03 -0500

    updated toggle text size

--- KeyBoardCSharp/KeyBoardCSharp/SideBar.Designer.cs
+++ KeyBoardCSharp/KeyBoardCSharp/SideBar.Designer.cs
@@ -28,84 +28,89 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.button1 = new System.Windows.Forms.Button();
-      this.button2 = new System.Windows.Forms.Button();
-      this.button3 = new System.Windows.Forms.Button();
-      this.button4 = new System.Windows.Forms.Button();
-      this.SuspendLayout();
-      // 
-      // button1
-      // 
-      this.button1.BackColor = System.Drawing.Color.DimGray;
-      this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-      this.button1.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.button1.ForeColor = System.Drawing.Color.White;
-      this.button1.Location = new System.Drawing.Point(6, 260);
-      this.button1.Name = "button1";
-      this.button1.Size = new System.Drawing.Size(69, 61);
-      this.button1.TabIndex = 0;
-      this.button1.Text = "Close";
-      this.button1.UseVisualStyleBackColor = false;
-      this.button1.Click += new System.EventHandler(this.button1_Click);
-      // 
-      // button2
-      // 
-      this.button2.BackColor = System.Drawing.Color.DimGray;
-      this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-      this.button2.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.button2.ForeColor = System.Drawing.Color.White;
-      this.button2.Location = new System.Drawing.Point(6, 12);
-      this.button2.Name = "button2";
-      this.button2.Size = new System.Drawing.Size(69, 61);
-      this.button2.TabIndex = 1;
-      this.button2.Text = "Toggle";
-      this.button2.UseVisualStyleBackColor = false;
-      this.button2.Click += new System.EventHandler(this.button2_Click);
-      // 
-      // button3
-      // 
-      this.button3.BackColor = System.Drawing.Color.DimGray;
-      this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-      this.button3.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.button3.ForeColor = System.Drawing.Color.White;
-      this.button3.Location = new System.Drawing.Point(6, 79);
-      this.button3.Name = "button3";
-      this.button3.Size = new System.Drawing.Size(69, 61);
-      this.button3.TabIndex = 2;
-      this.button3.Text = "Hide Bar";
-      this.button3.UseVisualStyleBackColor = false;
-      this.button3.Click += new System.EventHandler(this.button3_Click);
-      // 
-      // button4
-      // 
-      this.button4.BackColor = System.Drawing.Color.DimGray;
-      this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-      this.button4.Font = new System.Drawing.Font("Arial", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.button4.ForeColor = System.Drawing.Color.White;
-      this.button4.Location = new System.Drawing.Point(6, 146);
-      this.button4.Name = "button4";
-      this.button4.Size = new System.Drawing.Size(69, 61);
-      this.button4.TabIndex = 3;
-      this.button4.Text = "Opacity";
-      this.button4.UseVisualStyleBackColor = false;
-      this.button4.Click += new System.EventHandler(this.button4_Click);
-      // 
-      // SideBar
-      // 
-      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.BackColor = System.Drawing.Color.Black;
-      this.ClientSize = new System.Drawing.Size(82, 330);
-      this.Controls.Add(this.button4);
-      this.Controls.Add(this.button3);
-      this.Controls.Add(this.button2);
-      this.Controls.Add(this.button1);
-      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-      this.Name = "SideBar";
-      this.ShowInTaskbar = false;
-      this.Text = "SideBar";
-      this.Load += new System.EventHandler(this.SideBar_Load);
-      this.ResumeLayout(false);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(4, 211);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial", 7F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(4, 10);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Toggle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DimGray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Arial", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(4, 64);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(52, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Hide Bar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.DimGray;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Arial", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(4, 119);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(52, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Opacity";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // SideBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(62, 268);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "SideBar";
+            this.ShowInTaskbar = false;
+            this.Text = "SideBar";
+            this.Load += new System.EventHandler(this.SideBar_Load);
+            this.ResumeLayout(false);
 
     }
 
